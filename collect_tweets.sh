34d0b4f8d13d440be74fc25e182389eb16508349
lyraxvincent <njongevincent@gmail.com>
2021-07-25 18:57:15 +0300

    filter retweets

--- collect_tweets.sh
+++ collect_tweets.sh
@@ -1,1 +1,1 @@
-twint -s COVID19 --lang en --since 2020-03-25 -o COVID19_25July2021.csv --csv
+twint -s COVID19 --lang en --since 2020-03-25 --until 2020-03-30 --filter-retweets -o COVID19_25July2021.csv --csv
