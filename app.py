e3281170261aad0c19c2b2506718a2548c322a55
lyraxvincent <njongevincent@gmail.com>
2023-06-28 21:23:01 +0300

    Hide page elements

--- app.py
+++ app.py
@@ -3,6 +3,39 @@ import pickle
 import pandas as pd
 import numpy as np
 
+# page settings
+st.set_page_config(layout="centered", page_icon="💬", page_title="sentlysis")
+st.write(
+    """
+    <style>
+    [data-testid="stMetricDelta"] svg {
+        display: none;
+    }
+    </style>
+    """,
+    unsafe_allow_html=True)
+# hide "made with streamlit"
+st.write("""
+         <style>
+         footer {visibility: hidden;}
+         </style>
+         """, unsafe_allow_html=True)
+# hide header elements
+st.markdown("""
+            <style>
+            #MainMenu, header {visibility: hidden;}
+            </style>
+            """, unsafe_allow_html=True)
+# reduce whitespace pagetop
+st.markdown(
+    """
+    <style>
+    div.block-container {padding-top:1rem;}
+    </style>
+    """, unsafe_allow_html=True
+)
+
+# app logic
 labels_dict = {-1: "Negative 😣", 0: "Neutral 😌", 1: "Positive 😊"}
 
 text = st.text_input(label="Input text:", value="")
@@ -17,4 +50,5 @@ with st.container():
         st.caption("Predicted sentiment: ")
 
     with col2:
-        st.markdown(f":green[{pd.Series(prediction).map(labels_dict).values[0]}]")
+        if text.strip() != "":
+            st.markdown(f":green[{pd.Series(prediction).map(labels_dict).values[0]}]")
